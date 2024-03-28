5275c480e94466f55c83766da6caac7f90be0644
Anthony Vallee <tony.vallee@hotmail.com>
2024-03-28 08:26:45 -0400

    feat: download tool from GitHub , minimal arch/platform detection

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,8 +2,7 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for redpanda-rpk.
-GH_REPO="https://github.com/anthonyvallee/asdf-redpanda-rpk"
+GH_REPO="https://github.com/redpanda-data/redpanda"
 TOOL_NAME="redpanda-rpk"
 TOOL_TEST="rpk --help"
 
@@ -14,7 +13,7 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if redpanda-rpk is not hosted on GitHub releases.
+# NOTE: You might want to remove this if rpk is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
@@ -26,25 +25,60 @@ sort_versions() {
 
 list_github_tags() {
 	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
+		grep -Eo 'refs/tags/v\d+\.\d+\.\d+$' |
+		cut -d/ -f3- |
 		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if redpanda-rpk has other means of determining installable versions.
 	list_github_tags
 }
 
+get_arch() {
+	local arch
+
+	arch=$(uname -m)
+
+	case $arch in
+	amd64 | x86_64)
+		echo "amd64"
+		;;
+	arm64)
+		echo "arm64"
+		;;
+	*)
+		echo ""
+		;;
+	esac
+}
+
+get_platform() {
+	[ "Linux" = "$(uname)" ] && echo "linux" || echo "darwin"
+}
+
 download_release() {
-	local version filename url
+	local version filename arch platform archive url
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for redpanda-rpk
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	arch=$(get_arch)
+	if [ -z "$arch" ]; then
+		fail "Unsupported architecture: $arch"
+	fi
+	echo "Detected architecture: $arch"
+
+	platform=$(get_platform)
+	if [ -z "$platform" ]; then
+		fail "Unsupported platform: $platform"
+	fi
+	echo "Detected platform: $platform"
+
+	archive="rpk-$platform-$arch.zip"
+	url="$GH_REPO/releases/download/v$version/$archive"
 
 	echo "* Downloading $TOOL_NAME release $version..."
+	echo "* Fetching release asset ${archive} on GitHub..."
+
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
 
@@ -61,7 +95,7 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert redpanda-rpk executable exists.
+		# TODO: Assert rpk executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
